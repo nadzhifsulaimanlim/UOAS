43d7406f98839cef925bb356ccdcf2d50a93133d
nadzhif <nadzhifsulaimanlim@gmail.com>
2019-08-06 19:52:03 +0800

    okay

--- app/src/main/java/com/apek/uoas/MainActivity.java
+++ app/src/main/java/com/apek/uoas/MainActivity.java
@@ -1,12 +1,15 @@
 package com.apek.uoas;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
 
 
+    private FirebaseDatabase firebaseDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
